33753439cac9e50f5509c1d5c1e17fd1f1082143
Orion Gunning <orion.gunning@gmail.com>
2022-01-13 08:56:12 +1100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -10,13 +10,17 @@ const PORT = process.env.PORT || 5000;
 ////////////////    START APP CLIENT      //////////////////////////
 
 const app = express();
-
+app.get('/', (req, res) => {
+    console.error('express connection');
+    res.sendFile(path.join(__dirname, '../client/dist'));
+});
+/*
 // Priority serve any static files.
 app.use(serveStatic(path.resolve(__dirname, '../client/dist')));
 
 app.listen(PORT, function () {
     console.log(`Our app is running on port ${ PORT }`);
-});
+});*/
 
 ////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////
