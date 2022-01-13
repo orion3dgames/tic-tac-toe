99014bfb684f7a2eef63bcf17200d36bab0c058d
Orion Gunning <orion.gunning@gmail.com>
2022-01-13 11:20:22 +1100

    save

--- client/src/main.js
+++ client/src/main.js
@@ -12,14 +12,10 @@ import Default from "./layouts/Default";
 Vue.component('default_layout', Default);
 
 // START SOCKET IO
-console.log('https://tictactoe-vuejs.herokuapp.com');
 import VueSocketIO from 'vue-socket.io'
 Vue.use(new VueSocketIO({
   debug: false,
-  //connection: window.location+':5000',
-  connection: 'https://tictactoe-vuejs.herokuapp.com:'+process.env.PORT,
-  //connection: 'https://tictactoe-vuejs.herokuapp.com',
-  //connection: 'http://localhost:5000',
+  connection: '/',
   options: {
     'reconnectionDelay': 1000,
     'reconnectionDelayMax' : 5000,
